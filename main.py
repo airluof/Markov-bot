99c6979780eb9c736b9e6d205813ab7143baf54b
airluof <etofleks@gmail.com>
2024-11-01 17:52:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,16 @@ import os
 import random
 import re
 import time
-import io
-
 import aiofiles
 import aiogram
 import dateparser
 import dotenv
 import markovify
-from aiogram import Dispatcher, types
-from aiogram.types import InputFile
-from PIL import Image, ImageDraw, ImageFont
+from aiogram import Dispatcher
+from aiogram.types import Message as MessageType, InputFile
 from loguru import logger
-
 from middleware import BotMessagesMiddleware
-from mem import populate_base  # Импорт функции для инициализации базы
+from mem import populate_base, register_handlers  # Импортируйте функцию для регистрации
 
 dotenv.load_dotenv()
 
@@ -30,90 +26,96 @@ BOT = aiogram.Bot(
 )
 DP = Dispatcher(BOT)
 
-USER_BASE = {}
-
-async def onBotStart(dp: Dispatcher) -> None:
-    global USER_BASE
-    logger.info("Бот запущен!")
-    USER_BASE = await load_db()
-
-async def load_db(folder="db"):
-    # Загрузка базы данных
-    dict_to_return = {}
-    for filename in os.listdir(folder):
-        if filename.endswith(".jsonc"):
-            async with aiofiles.open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
-                contents = await file.read()
-                dict_to_return[filename[:-6]] = json.loads(contents)
-    return dict_to_return
-
-async def save_db(folder="db"):
-    for chat_id, data in USER_BASE.items():
-        async with aiofiles.open(os.path.join(folder, f"{chat_id}.jsonc"), 'w', encoding='utf-8') as file:
-            await file.write(json.dumps(data, ensure_ascii=False))
-
+# Настройка middleware
 DP.middleware.setup(BotMessagesMiddleware(populate_base))
 
+USER_BASE = {}
+
+# Регистрация обработчиков
+register_handlers(DP)
+
 @DP.message_handler(commands=["start"])
-async def hello_handler(msg: types.Message):
-    await msg.answer("Отлично! 😊\n\nЧто бы я работал, тебе нужно выдать мне права администратора. 🫂")
+async def hello_handler(msg: MessageType):
+    await msg.answer("Отлично! 😊\n\nЧто бы я работал, тебе нужно выдать мне права администратора. 🫂\nПосле выдачи я сумею работать как положено 👀")
 
 @DP.chat_join_request_handler()
 @DP.message_handler(content_types=["new_chat_members", "group_chat_created", "supergroup_chat_created"])
-async def chat_join_handler(msg: types.Message):
+async def chat_join_handler(msg: MessageType):
     bot_id = (await BOT.get_me()).id
     if not any([i for i in msg.new_chat_members if i.id == bot_id]):
         return
     await msg.answer("<b>Привет! 🙋</b>\n\nСпасибо за то что добавил меня к себе в беседу.\nДля моей работы выдай мне права администратора.")
 
 @DP.message_handler(commands=["stats", "statistics", "stat"])
-async def stats_handler(msg: types.Message):
+async def stats_handler(msg: MessageType):
     await msg.answer(f"<b>Статистика ℹ️</b>\n\nВ базе данных этой группы хранится <code>{len(USER_BASE[str(msg.chat.id)]['Messages'])}</code> сообщений.")
 
-async def check_admin(msg: types.Message):
+async def check_admin(msg: MessageType):
     if not (await BOT.get_chat_member(msg.chat.id, msg.from_user.id)).is_chat_admin():
         await msg.answer("⚠️ Данную команду могут использовать только <b>администраторы чата</b>.")
         return False
     return True
 
 @DP.message_handler(commands=["disable", "turn_off", "turnoff", "off"])
-async def disable_handler(msg: types.Message):
+async def disable_handler(msg: MessageType):
     if not await check_admin(msg):
         return
 
     args = (msg.get_args() or "")
     disabled_seconds = 0 
 
-    parsed = dateparser.parse(args) if args else None
-    if parsed:
-        disabled_seconds = (parsed.timestamp() - datetime.now().timestamp())
+    parsed = None
+    if args:
+        parsed = dateparser.parse(args)
     else:
-        disabled_seconds = 7 * 24 * 60 * 60  # по умолчанию 7 дней
+        disabled_seconds = 7 * 24 * 60 * 60
 
-    if disabled_seconds < 120:  # минимум 2 минуты
-        await msg.answer("Пожалуйста, укажи время, на которое нужно отключить генерацию сообщений.")
+    if parsed:
+        disabled_seconds = (datetime.now().timestamp() - parsed.timestamp())
+
+    if disabled_seconds < 2 * 60:
+        await msg.answer(f"Пожалуйста, укажи время на которое нужно выключить генерацию сообщений. Указанное тобою значение (<code>{seconds_to_userfriendly_string(disabled_seconds)}</code>) уж слишком мало.\n\nПример: <code>/disable 5 часов</code>")
         return
 
-    await msg.answer(f"Окей, я отключу генерацию сообщений на <code>{seconds_to_userfriendly_string(disabled_seconds)}</code>.")
-    update_record(msg.chat.id, {"OffUntil": int(time.time()) + int(disabled_seconds)})
+    await msg.answer(f"Окей, я отключу генерацию сообщений в этой беседе на <code>{seconds_to_userfriendly_string(disabled_seconds)}</code>.\n\nℹ️ Администраторы могут меня включить, прописав команду /enable.")
+    update_record(
+        msg.chat.id,
+        {
+            "OffUntil": int(time.time()) + int(disabled_seconds)
+        }
+    )
 
 @DP.message_handler(commands=["enable", "turn_on", "turnon", "on"])
-async def enable_handler(msg: types.Message):
+async def enable_handler(msg: MessageType):
     if not await check_admin(msg):
         return
 
-    update_record(msg.chat.id, {"OffUntil": 0})
-    await msg.answer("Окей, теперь я снова могу генерировать сообщения! 🙂")
+    await msg.answer(
+        "Окей, теперь я снова могу генерировать сообщения! 🙂"
+        if USER_BASE[str(msg.chat.id)]["OffUntil"] else
+        "Дятел! Я и так включён, так как не был отключён администраторами беседы до этого."
+    )
+
+    update_record(
+        msg.chat.id,
+        {
+            "OffUntil": 0
+        }
+    )
 
 @DP.message_handler()
-async def message_handler(msg: types.Message):
+async def message_handler(msg: MessageType):
     message_text = msg.text or msg.caption
-    add_database_message(msg.chat.id, message_text)
+    add_database_message(
+        msg.chat.id,
+        message_text
+    )
 
-    if USER_BASE[str(msg.chat.id)]["OffUntil"] > int(time.time()):
+    if USER_BASE[str(msg.chat.id)]["OffUntil"] > int(time.time()) and USER_BASE[str(msg.chat.id)]["OffUntil"]:
         return
 
     is_triggered = any([i for i in ["макс", "max", "@maxzens_bot"] if i in message_text.lower()])
+
     if msg.reply_to_message and not is_triggered:
         is_triggered = msg.reply_to_message.from_user.id == BOT.id
 
@@ -128,49 +130,15 @@ async def message_handler(msg: types.Message):
 
     if msg_generated:
         msg_generated = re.sub(r"@(\w*)", "<i><a href=\"https://t.me/\\1\">@\\1</a></i>", msg_generated)
+
         await asyncio.sleep(random.uniform(0, 1))
         await msg.bot.send_chat_action(msg.chat.id, "typing")
         await asyncio.sleep(random.uniform(1, 3))
 
         if random.randint(0, 1):
-            await msg.answer(msg_generated, disable_notification=True)
+            await msg.answer(msg_generated, disable_notification=True, allow_sending_without_reply=True, disable_web_page_preview=True)
         else:
-            await msg.reply(msg_generated, disable_notification=True)
-
-def seconds_to_userfriendly_string(seconds):
-    # Функция для преобразования секунд в человекочитаемый формат
-    seconds = int(seconds)
-    if seconds < 0:
-        seconds = -seconds
-    days, seconds = divmod(seconds, 86400)
-    hours, seconds = divmod(seconds, 3600)
-    minutes, seconds = divmod(seconds, 60)
-    parts = []
-    if days: parts.append(f"{days} дн." if days > 1 else "1 дн.")
-    if hours: parts.append(f"{hours} ч." if hours > 1 else "1 ч.")
-    if minutes: parts.append(f"{minutes} мин." if minutes > 1 else "1 мин.")
-    if seconds: parts.append(f"{seconds} сек." if seconds > 1 else "1 сек.")
-    return ", ".join(parts)
-
-def add_database_message(chat_id: int, message_text: str):
-    message_text = message_text.strip()
-    if not message_text:
-        return
-
-    if str(chat_id) not in USER_BASE:
-        USER_BASE[str(chat_id)] = {"Messages": [], "OffUntil": 0}
-
-    USER_BASE[str(chat_id)]["Messages"].append(message_text)
-
-def update_record(chat_id: int, new_value: dict):
-    if str(chat_id) not in USER_BASE:
-        USER_BASE[str(chat_id)] = {}
-    USER_BASE[str(chat_id)].update(new_value)
-
-async def bg_saver():
-    while True:
-        await asyncio.sleep(60)  # Сохранять базу раз в минуту
-        await save_db()
+            await msg.reply(msg_generated, disable_notification=True, allow_sending_without_reply=True, disable_web_page_preview=True)
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
